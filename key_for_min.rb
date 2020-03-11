4656c8eb5ee2129a2d81d92d075b7c146b21d311
Austin Redmond <aredmond85@gmail.com>
2020-03-11 04:06:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,16 +3,15 @@
 
 def key_for_min_value(name_hash)
   # code goes here
-  min_value = nil
-  min_key = nil
+  m_value = nil
+  m_key = nil
   name_hash.each do |name, number|
-    if min_value == nil
-      min_value = number
-      min_key = name
-    elsif min_value > number
-      min_value = number
-      min_key = name
+      m_key = name if min_value == nil
+      m_value = number if min_value == nil
+    if min_value > number
+      m_value = number
+      m_key = name
     end
   end
-  min_key
+  m_key
 end
